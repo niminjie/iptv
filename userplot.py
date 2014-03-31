746fe415e3480b13e5a3cb6e3a117ce3d3f8f601
niminjie <niminjiecide@gmail.com>
2014-03-31 16:55:46 +0800

    generate interval point

--- userplot.py
+++ userplot.py
@@ -15,8 +15,40 @@ def main():
     test()
 
 def plot(user):
-    # user : [starttime,endtime,timespan]
-    pass
+    ''' 
+    user : [starttime,endtime,timespan]
+    '''
+    # Generate y axis
+    span = 10
+    y = [0 for i in range(60 / span * 24)]
+
+    # [[0,10], [10,20], [20,30],...]
+    intervals = []
+    for i in range(60 / span * 24):
+        intervals.append([i * 10, (i + 1) * 10])
+
+    for t in user:
+        start_min = conver_to_minute(t[0])
+        end_min = conver_to_minute(t[1])
+
+        s_idx = 0
+        while(start_min >= intervals[s_idx][1]):
+            s_idx += 1
+
+        e_idx = s_idx
+        while(end_min >= intervals[e_idx][0]):
+            e_idx += 1
+
+        for i in range(s_idx, e_idx):
+            y[i] += 1
+    print y
+
+
+def conver_to_minute(time):
+    hour = int(time.split(' ')[1].split(':')[0])
+    minutes = int(time.split(' ')[1].split(':')[1])
+    #print hour, minutes
+    return hour * 60 + minutes
 
 
 def test():
@@ -30,6 +62,7 @@ def test():
             user_info.setdefault(user_id,[])
             user_info[user_id].append([start_time, end_time, time_span.strip()])
     # user_info['1'] can get all the infomation
+    plot(user_info['1'])
 
 if __name__ == '__main__':
    main() 
